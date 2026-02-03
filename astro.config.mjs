2adbdb8ec68a64368cbc8db89c9a21fb102b410c
siddharth-09 <1234siddharth.panchal@gmail.com>
2026-02-03 21:15:55 +0530

    MobileStarting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    server: {
+      allowedHosts: [
+        'disciplines-proceeds-filtering-these.trycloudflare.com'
+      ]
+    }
+  }
+});
